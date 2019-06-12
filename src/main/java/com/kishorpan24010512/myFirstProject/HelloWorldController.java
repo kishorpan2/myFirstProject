3f33ca86d633f918a222375d8cca5eac70ba5b98
Kishor Pandey <kishor.jpandey@gmail.com>
2019-06-12 09:41:25 -0700

    first feature

--- src/main/java/com/kishorpan24010512/myFirstProject/HelloWorldController.java
+++ src/main/java/com/kishorpan24010512/myFirstProject/HelloWorldController.java
@@ -2,11 +2,23 @@ package com.kishorpan24010512.myFirstProject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public class HelloWorldController {
-    @GetMapping("/hello/name")
-    public String getHelloToMe(@PathVariable String name,@PathVariable String from){
-        return "Hello, " + name + "from ";
+    // specify route method for this method
+
+    @GetMapping("/")
+
+    public String getHelloWorld(){
+        return "Hello World " ;
+    }
+
+    @GetMapping("/hello/{name}")
+
+    public String getHelloToMe(@PathVariable String name){
+        return "Hello, " + name;
     }
 
 }
