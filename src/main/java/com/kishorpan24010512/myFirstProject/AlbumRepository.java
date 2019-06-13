0e0cee13c365cd70299482dd45c4956aa13ab3a7
Kishor Pandey <kishor.jpandey@gmail.com>
2019-06-13 13:39:25 -0700

    still working

---
+++ src/main/java/com/kishorpan24010512/myFirstProject/AlbumRepository.java
@@ -0,0 +1,8 @@
+package com.kishorpan24010512.myFirstProject;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlbumRepository extends CrudRepository<Album, Long> {
+
+}
